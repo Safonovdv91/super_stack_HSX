6b3dd0c7a121ebd386688f1d16eac3ae7937d6db
Safonov <softik.d@gmail.com>
2023-07-14 16:14:50 +0300

    Реализован класс с манки-тестами, и немного юниттестов.

--- main.py
+++ main.py
@@ -1,7 +1,80 @@
+"""
+Напишите класс SuperStack, который реализует структуру данных "стек".
+Использовать стандартные списковые типы Питона (list, deque и т.д.) для хранения элементов стека нельзя.
+Вместо этого определите класс SuperStackElement и храните в его объектах значение элемента и объект предыдущего элемента.
+
+У класса SuperStack должны быть следующие методы:
+
+- push (добавляет значение на верхушку стека)
+- peek (возвращает значение с верхушки стека)
+- pop (возвращает и удаляет значение с верхушки стека)
+
+"""
+
+
+class SuperStackElement:
+    """
+    Класс созданый для реализации работы класса SuperStack, принимает 2 значения, последний и предпоследний элементы.
+    """
+
+    def __init__(self, el=None, old_el=None):
+        self.old_el = old_el
+        self.el = el
+
+
+class SuperStack:
+    """
+    Класс имитирующий работу стэка.
+    """
+    def __init__(self):
+        self.ss_element = None
+
+    def push(self, element):
+        self.ss_element = SuperStackElement(element, self.ss_element)
+
+    def peek(self):
+        if self.ss_element is None:
+            return None
+        return self.ss_element.el
+
+    def pop(self):
+        if self.ss_element is None:
+            return None
+        pop_element = self.ss_element
+        old_element = self.ss_element.old_el
+        if old_element is None:
+            self.ss_element = None
+            return pop_element.el
+        self.ss_element = SuperStackElement(old_element, old_element.old_el)
+        return pop_element.el
+
 
 def main():
-    pass
+    ss = SuperStack()
+
+    print(ss.push(110))
+    print(ss.push(111))
+    print(ss.push(112))
+    print(ss.push(113))
+    print(ss.push(114))
+    print(ss.push(115))
+    print(ss.peek())    #115
+    print(ss.pop())     #115
+    print(ss.pop())     #114
+    print(ss.pop())     #113
+    print(ss.peek())    #112
+    print(ss.pop())     #112
+    print(ss.pop())     #111
+    print(ss.pop())     #110
+    print(ss.peek())    # none
+    print(ss.pop())     # none
+    print(ss.push([12, 12]))
 
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
--- tests.py
+++ tests.py
@@ -5,3 +5,50 @@ sys.path.append(os.getcwd())
 
 import main
 
+
+class TestSuperStackPush(unittest.TestCase):
+
+    def test_SS_push(self):
+        "Теструем что пушит без проблем и не падает с ошибкой"
+        ss = main.SuperStack()
+        self.assertEqual(ss.push(12), None)
+        self.assertEqual(ss.push("123"), None)
+        self.assertEqual(ss.push(["123", 412]), None)
+        self.assertEqual(ss.push({"sd": 1245}), None)
+        self.assertEqual(ss.push(("touple", "touple")), None)
+
+    def test_SS_peek(self):
+        "Проверка корректных значений"
+        ss = main.SuperStack()
+
+        self.assertEqual(ss.peek(), None)
+        ss.push(1)
+        ss.push(2)
+        ss.push(3)
+        self.assertEqual(ss.peek(), 3)
+        ss.push([12, 12])
+        self.assertEqual(ss.peek(), [12, 12])
+        ss.push("qweq")
+        self.assertEqual(ss.peek(), "qweq")
+        ss.push([])
+        self.assertEqual(ss.peek(), [])
+
+    def test_SS_pop(self):
+        "Проверка корректных значений"
+        ss = main.SuperStack()
+
+        self.assertEqual(ss.pop(), None)
+        self.assertEqual(ss.pop(), None)
+        ss.push(12)
+        self.assertEqual(ss.pop(), 12)
+        self.assertEqual(ss.peek(), None)
+        ss.push(11)
+        ss.push(["12", 13])
+        ss.push(13)
+        self.assertEqual(ss.pop(), 13)
+        self.assertEqual(ss.peek(), ["12", 13])
+        self.assertEqual(ss.pop(), ["12", 13])
+        self.assertEqual(ss.pop(), 11)
+        self.assertEqual(ss.pop(), None)
+        self.assertEqual(ss.pop(), None)
+        self.assertEqual(ss.peek(), None)
